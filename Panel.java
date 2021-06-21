41191e91e86bb7969afe90248b4060c36aaad7af
vtv090 <65129688+vtv090@users.noreply.github.com>
2021-06-21 23:43:00 +0200

    Update Panel.java

--- Panel.java
+++ Panel.java
@@ -229,9 +229,9 @@ public class Panel extends JPanel implements Runnable, KeyListener {
             Entity e = new Entity(SIZE);
             e.setPosition(-100, -100);
             snake.add(e);
-            if (score % 10 == 0) {
+            if (score % 5 == 0) {
                 level++;
-                if (level > 10) level = 10;
+                if (level > 5) level = 5;
                 setFPS(level * 4);
             }
         }
